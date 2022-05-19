e8d8ff25caa6e2759a11c3fe3b0c069e89b17ab9
Yaroslav <arist@MacBook-Pro-Arist.local>
2022-05-19 14:10:55 +0300

    Dirty version of authorization

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
 const taskRouter = require('./routes/task-routers');
 const apiRouter = require('./routes/api-routers');
 const methodOverride = require('method-override');
+const api_auth_routes = require('./routes/api-auth-routes');
+const secureRoute = require('./routes/secure-auth-routes');
 
 const app = express();
 const PORT = 3000;
 const db = 'mongodb+srv://Yaroslav:qwerrewq@clus.mnkjs.mongodb.net/NodeTask?retryWrites=true&w=majority'
+
+require('./auth/auth');
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -19,9 +25,13 @@ app.use(express.urlencoded({extended: false }));
 app.use(express.static('styles'));
 app.use(methodOverride('_method'));
 
+app.use(taskRouter);
+app.use(apiRouter);
+
+app.use('/api/auth', api_auth_routes);
+app.use('/api/user', passport.authenticate('jwt', { session: false }), secureRoute);
+
+
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
   });
-
-app.use(taskRouter );
-app.use(apiRouter);
