734b3d9f51e760fc699fb326b3af5ed8587e4f2c
Yaroslav <arist@MacBook-Pro-Arist.local>
2022-05-24 14:27:34 +0300

    First commit

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { Server } = require("socket.io");
+
 const taskRouter = require('./routes/task-routers');
 const apiRouter = require('./routes/api-routers');
 const methodOverride = require('method-override');
 
 const app = express();
 const PORT = 3000;
-const db = 'mongodb+srv://Yaroslav:qwerrewq@clus.mnkjs.mongodb.net/NodeTask?retryWrites=true&w=majority'
+const db = 'mongodb+srv://Yaroslav:qwerrewq@clus.mnkjs.mongodb.net/NodeTask?retryWrites=true&w=majority';
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
-  .then((res) => console.log('Connected to DB'))
+  .then(() => console.log('Connected to DB'))
   .catch((error) => console.log(error));
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false }));
 app.use(express.static('styles'));
+app.use(express.static('node_modules'));
 app.use(methodOverride('_method'));
+
+const server = require('http').createServer(app);
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
   });
 
-app.use(taskRouter );
+app.use(taskRouter);
 app.use(apiRouter);
+
