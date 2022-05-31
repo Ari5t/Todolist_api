cb74b067f3ff895dcb44e71d1996391b446da54e
Yaroslav <arist@MacBook-Pro-Arist.local>
2022-05-31 15:10:56 +0300

    add DB_URL in .env

--- server.ts
+++ server.ts
@@ -14,14 +14,12 @@ import apiRouter from './routes/api-routers'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const db = 'mongodb+srv://Yaroslav:qwerrewq@clus.mnkjs.mongodb.net/NodeTask?retryWrites=true&w=majority'
 
 const server = http.createServer(app)
 const io = new Server(server)
 
-
 try{
-  mongoose.connect(db)
+  mongoose.connect(`${process.env.DB_URL}`)
   // , { useNewUrlParser: true, useUnifiedTopology: true }
   console.log('Connected to DB')
 }catch (error){
