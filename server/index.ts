74d7ff33c2e94c86f3e60acf9f1b9876093de6c8
Yaroslav <arist@MacBook-Pro-Arist.local>
2022-06-05 12:58:15 +0300

    added parameters to .env.defaults

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,20 @@ import mongo from "./mongodb"
 import 'dotenv/config'
 import "dotenv-defaults/config"
 
-const url = `${process.env.DB_PROTOCOL}${process.env.DB_USERNAME }:${process.env.DB_PASSWORD}${process.env.DB_CLUSTER }.mongodb.net/${process.env.DB_NAME }${process.env.DB_PARAMETERS}`;
+const env = process.env; 
+
+const url = `${env.DB_PROTOCOL}${env.DB_USERNAME}:${env.DB_PASSWORD}${env.DB_CLUSTER}.mongodb.net/${env.DB_NAME}${env.DB_PARAMETERS}`;
+
 
 const main = async () => {
-    await mongo(url)
-    require('./socket')
-    require('./app')
-    require('./http')
-  }
-  
-  main()
+  await mongo(url)
+  require('./socket')
+  require('./app')
+  require('./http')
+}
+
+main()
+
+// import './socket'
+// import './app'
+// import './http'
